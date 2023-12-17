e3463f77156951d428ed4eb66f878fb145674a10
Mmaduabuchi Elvis Igwilo <148869665+Elhefeelvis1@users.noreply.github.com>
2023-12-17 14:21:05 +0100

    Made some minor modifications to the buttons and cart-script.js

--- js/cart-script.js
+++ js/cart-script.js
@@ -1,21 +1,17 @@
 let cart = document.querySelector(".cart-section");
 let checkout = document.querySelector(".checkout-section");
 let approved = document.querySelector(".approved-section");
-let cartBtn = document.querySelector(".cart-btn");
-let checkoutBtn = document.querySelector(".checkout-btn");
-let approvedBtn = document.querySelector(".approved-btn");
-
 
 window.addEventListener('click', (e) => {
-    if(e.target === cartBtn){
+    if(e.target.classList.contains("cart-btn")){
         cart.style.display = "none";
         checkout.style.display ="block";
     }
-    if(e.target === checkoutBtn){
+    if(e.target.classList.contains("checkout-btn")){
         checkout.style.display = "none";
         approved.style.display = "block";
     }
-    if(e.target === approvedBtn){
+    if(e.target.classList.contains("approved-btn")){
         window.location.replace('index.html');
     }
 })

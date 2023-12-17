6f832389cdb093340676ebbff8974c9c97b87f46
Mmaduabuchi Elvis Igwilo <148869665+Elhefeelvis1@users.noreply.github.com>
2023-12-17 14:02:14 +0100

    Made my function a bit more concise in cart-script.js

--- js/cart-script.js
+++ js/cart-script.js
@@ -6,17 +6,16 @@ let checkoutBtn = document.querySelector(".checkout-btn");
 let approvedBtn = document.querySelector(".approved-btn");
 
 
-cart.addEventListener('click', (e) => {
-    console.log(e);
-    if(e.target == cartBtn){
+window.addEventListener('click', (e) => {
+    if(e.target === cartBtn){
         cart.style.display = "none";
-        checkout.style.display = "block";
+        checkout.style.display ="block";
     }
-})
-checkoutBtn.addEventListener('click', () => {
-    checkout.style.display = "none";
-    approved.style.display = "block";
-})
-approvedBtn.addEventListener('click', () => {
-    window.location.replace('product-review.html');
+    if(e.target === checkoutBtn){
+        checkout.style.display = "none";
+        approved.style.display = "block";
+    }
+    if(e.target === approvedBtn){
+        window.location.replace('index.html');
+    }
 })
